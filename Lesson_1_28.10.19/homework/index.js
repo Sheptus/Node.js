b0a9f807adc12c3bf958a1d91aedbc1eee4f313b
Sheptus <sergey.sheptus@gmail.com>
2019-11-01 14:56:17 +0200

    HomeWork_Lesson_1

---
+++ Lesson_1_28.10.19/homework/index.js
@@ -0,0 +1,10 @@
+module.exports = {
+    writeToFile: (filePath, str) => {
+        fs.appendFile(filePath, str + "\n", (err) => {
+            if (err) console.log(err)
+        })
+    },
+    writeToFileSync: (filePath, str) => {
+        return fs.appendFileSync(filePath, str + "\n")
+    }
+}
