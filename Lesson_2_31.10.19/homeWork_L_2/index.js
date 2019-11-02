41a97ed06439bcd6e4ff40378dbf9317388667e1
Sheptus <sergey.sheptus@gmail.com>
2019-11-02 16:23:35 +0200

    c

--- Lesson_2_31.10.19/homeWork_L_2/index.js
+++ Lesson_2_31.10.19/homeWork_L_2/index.js
@@ -9,20 +9,20 @@ const cars = require('')
 const port = 4000;
 
 
-api.get("/Countries", (req, res) => {
-    console.log("start requesting for data...")
+// api.get("/Countries", (req, res) => {
+//     console.log("start requesting for data...")
 
-    const {
-        Country,
-        Capital
-    } = req.query;
+//     const {
+//         Country,
+//         Capital
+//     } = req.query;
 
-    if (!Country || !Capital) return res.send("Oppps something wrong! , please try again!!")
-    console.log(Country + " " + Capital)
-    return res.send(`Countries  Details: ${Country} , ${Capital}`);
-})
+//     if (!Country || !Capital) return res.send("Oppps something wrong! , please try again!!")
+//     console.log(Country + " " + Capital)
+//     return res.send(`Countries  Details: ${Country} , ${Capital}`);
+// })
 
-api.listen(port, (err) => {
-    if (err) console.log(err.message)
-    console.log(`Api listening to port:???? ${port} `)
-})
+// api.listen(port, (err) => {
+//     if (err) console.log(err.message)
+//     console.log(`Api listening to port:???? ${port} `)
+// })
