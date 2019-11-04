ef1ebf89a1ac75f40555e153e1036200e3fb591f
Sheptus <sergey.sheptus@gmail.com>
2019-11-04 17:40:24 +0200

    HW_Lesson_ 2

--- Lesson_2_31.10.19/homeWork_L_2/index.js
+++ Lesson_2_31.10.19/homeWork_L_2/index.js
@@ -1,28 +1,57 @@
-// const fs = require("fs")
-
-// console.log("script start")
-
-
 const express = require('express');
-const api = express();
-const cars = require('')
-const port = 4000;
+const cars = require('./cars.json');
+const logger = require('s_app_logger');
+
+const app = express();
+const port = 4023;
 
 
-// api.get("/Countries", (req, res) => {
-//     console.log("start requesting for data...")
 
-//     const {
-//         Country,
-//         Capital
-//     } = req.query;
 
-//     if (!Country || !Capital) return res.send("Oppps something wrong! , please try again!!")
-//     console.log(Country + " " + Capital)
-//     return res.send(`Countries  Details: ${Country} , ${Capital}`);
-// })
+app.get('/cars', (req, res, next) => {
+    console.log("start req...")
 
-// api.listen(port, (err) => {
-//     if (err) console.log(err.message)
-//     console.log(`Api listening to port:???? ${port} `)
-// })
+    const dateS = new Date();
+    const curDate = `${dateS.getDate()}:${(dateS.getMonth() + 1)}:${dateS.getFullYear()} - ${dateS.getHours()}:${dateS.getMinutes()}:${dateS.getSeconds()}`
+    const logTxt = `[${curDate}] ${req.get("host")}${req.originalUrl} `
+    res.send({
+        cars
+    });
+    logger.writeToFileSync("./log.txt", logTxt)
+    console.log(logTxt + "[" + curDate + "]");
+    // user send file to recive content
+    //download to download the file
+    console.log("res sended ...");
+    return res.download("Users\\sheptus\\Documents\\GitKraken\\Node.js\\Node.js\\Lesson_2_31.10.19\\homeWork_L_2\\log.txt")
+
+})
+
+app.get('/logs', (req, res, next) => {
+    console.log("start req to logs?mode=VALUE")
+    const {
+        cars
+    } = req.query;
+    if (!cars) return
+    res.send("Something went wrong!!")
+    logger.writeToFileSync("./log.txt", cars)
+    // user send file to recive content
+    //download to download the file
+    return res.send(cars)
+})
+
+app.get("/logs", (req, res, next) => {
+    const {
+        mode
+    } = req.query;
+    if (!mode) return res.send("error! no log request method")
+    if (mode === "file") return res.download("Users\\sheptus\\Documents\\GitKraken\\Node.js\\Node.js\\Lesson_2_31.10.19\\homeWork_L_2\\log.txt")
+    else if (mode === "content") res.sendFile("Users\\sheptus\\Documents\\GitKraken\\Node.js\\Node.js\\Lesson_2_31.10.19\\homeWork_L_2\\log.txt")
+    else return res.send("error! invalid log request method")
+})
+
+
+app.listen(port, (err) => {
+    if (err) console.log("Oppss some Error --:> " + err.message)
+    console.log(`Server running on http://localhost:${port} `)
+
+})
